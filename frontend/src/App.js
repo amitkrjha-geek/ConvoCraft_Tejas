f913147242381b9fda96b9483f1275c82f24da07
Amit Kumar Jha <amit.kumarjha.met19@itbhu.ac.in>
2024-01-06 15:55:47 +0530

    Intialized the paths and routings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { RouterProvider, CreateBrowserRouter } from 'react-router-dom';
+import Login from './components/Login';
+import Chat from './components/Chat';
 
 function App() {
+  const appRouter = CreateBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/chat",
+      element: <Chat />,
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={appRouter}/>
     </div>
   );
 }
