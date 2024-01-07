af9a1c7b23a3477c27109be0c766753734865624
Ankur Shukla <itzankurhere@gmail.com>
2024-01-07 20:04:29 +0530

    User Model imported and bug fixed

--- backend/controller/Auth/register.js
+++ backend/controller/Auth/register.js
@@ -10,8 +10,7 @@ const registerUser = async (req, res, next) => {
       name: Joi.string().required(),
       email: Joi.string().required(),
       password: Joi.string().required(),
-    });
-    // console.log(req.body);
+    });;
     const { error } = userSchema.validate(req.body);
     if (error) {
       console.log(error);
@@ -23,16 +22,10 @@ const registerUser = async (req, res, next) => {
       if (!email.includes("@")) {
         return res.status(400).json({ error: "Entered email address is not a valid email address." });
       }
-//   console.log(email);
       const isEmailExist = await User.exists({ email: email });
       if (isEmailExist) {
         return res.status(400).json({ error: "This user already exists." });
       }
-    //   const isUserNameExist = await User.exists({ userName: userName });
-    //   if (isUserNameExist) {
-    //     return res.status(400).json({ error: "This UserName already exists." });
-    //   }
-  
       const hashedPassword = await bycrypt.hash(password, 12);
       console.log(hashedPassword);
       User.create({ name: name, email: email, password: hashedPassword })
@@ -44,25 +37,25 @@ const registerUser = async (req, res, next) => {
             },
           };
           // sending mail to user
-          console.log("first");
+          // console.log("first");
           let success=true;
-          const data = {
-            email: email,
-            subject: "Verify your email address",
-            name:name,
-            otp: otp,
-            company: "Tech Developers",
-          };
-        //   await axios
-        //     .post(process.env.EMAIL_API_URL + "api/mail-verify", data, config)
-        //     .then((res) => {
-        //       success = true;
-        //     })
-        //     .catch((err) => {
-        //       success = false;
-        //       console.log(err)
-        //       console.log("error in sending mail to :" + email);
-        //     });
+          // const data = {
+          //   email: email,
+          //   subject: "Verify your email address",
+          //   name:name,
+          //   otp: otp,
+          //   company: "Tech Developers",
+          // };
+          // await axios
+          //   .post("https://mail-server-j1vc.onrender.com/" + "api/mail-verify", data, config)
+          //   .then((res) => {
+          //     success = true;
+          //   })
+          //   .catch((err) => {
+          //     success = false;
+          //     console.log(err)
+          //     console.log("error in sending mail to :" + email);
+          //   });
           // console.log("first1", success);
 
           // if (success) {
