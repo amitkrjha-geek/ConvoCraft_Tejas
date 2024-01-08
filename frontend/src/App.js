2f46b8e7fdfd31f4a91b0a94030c1f5e34e51666
Amit Kumar Jha <amit.kumarjha.met19@itbhu.ac.in>
2024-01-08 12:14:26 +0530

    Initilised the redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import { createBrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import Login from './components/Login';
 import Chat from './components/Chat';
+import store from './utils/store';
 
 function App() {
   const appRouter = createBrowserRouter([
@@ -17,9 +18,11 @@ function App() {
     }
   ]);
   return (
-    <div className="App">
-      <RouterProvider router={appRouter}/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <RouterProvider router={appRouter} />
+      </div>
+    </Provider>
   );
 }
 
