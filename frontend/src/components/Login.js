6a473e98bc122fd101a79821a203f779e874da69
Ankur Shukla <itzankurhere@gmail.com>
2024-01-08 19:51:19 +0530

    Auth Middleware, refresh route and signup and redux done

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,12 +2,16 @@ import React from 'react'
 import { useState, useRef } from 'react';
 import { Emailvalidate, Passwordvalidate } from '../constants/validate';
 import { Link, useNavigate } from 'react-router-dom';
+import { useSnackbar } from "notistack"
 import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 const Login = () => {
-  // const dispatch=useDispatch();
+  const {enqueueSnackbar}=useSnackbar();
+  const dispatch=useDispatch();
   const [showLogin, setShowLogin] = useState(true);
-  // const [errorMsgEmail, setErrorMsgEmail] = useState(null);
-  // const [errorMsgPassword, setErrorMsgPassword] = useState(null);
+  const [errorMsgEmail, setErrorMsgEmail] = useState(null);
+  const [errorMsgPassword, setErrorMsgPassword] = useState(null);
   const Email = useRef(null);
   const Password = useRef(null);
   const Name = useRef(null);
@@ -19,40 +23,64 @@ const formToggle=()=>{
   setShowLogin(!showLogin);
 }
  const clickHandler =()=>{
-  // const Emailmsg = Emailvalidate(Email.current.value);
-  //   const Passwordmsg = Passwordvalidate(Password.current.value);
-  //   setErrorMsgEmail(Emailmsg);
-  //   setErrorMsgPassword(Passwordmsg);
-  //   if (Emailmsg || Passwordmsg) return;
+  setErrorMsgEmail(Emailvalidate(Email));
+  setErrorMsgPassword(Passwordvalidate(Password));
     const config = {
       headers: {
         "Content-Type": "application/json",
       },
     };
-    const data = {
-      email: Email?.current?.value,
-      password: Password?.current?.value,
-    };
-    console.log(data);
-    // axios.post(process.env.REACT_APP_BACKEND_URL + "api/user/login", data, config).then((response) => {
-    //   // dispatch(setLoading(false))
-    //   console.log(response);
-    //   localStorage.setItem("userId", response.data.id)
-    //   localStorage.setItem("access_token", response.data.access_token)
-    //   localStorage.setItem("refresh_token", response.data.refresh_token)
-    //   // enqueueSnackbar("Logged in successfully", {
-    //   //   variant: 'success',
-    //   // });
-    //   navigate("/")
-    // })
-    //   .catch((error) => {
-    //     console.log("hi");
-    //     console.log(error);
-    //     // enqueueSnackbar("Invalid  Credentials", {
-    //     //   variant: 'error',
-    //     // });
-    //     // dispatch(setLoading(false))
-    //   });
+    if(showLogin){
+      const data = {
+        email: Email?.current?.value,
+        password: Password?.current?.value,
+      };
+      axios.post("http://localhost:5000/api/v1/login", data, config).then((response) => {
+        // dispatch(setLoading(false))
+        console.log(response);
+        localStorage.setItem("userId", response.data.id)
+        localStorage.setItem("access_token", response.data.access_token)
+        localStorage.setItem("refresh_token", response.data.refresh_token)
+        enqueueSnackbar("Logged in successfully", {
+          variant: 'success',
+        });
+        dispatch(addUser({userId: response.data.id}));
+        navigate("/chat");
+
+      })
+        .catch((error) => {
+          console.log("hi");
+          console.log(error);
+          enqueueSnackbar("Invalid  Credentials", {
+            variant: 'error',
+          });
+          // dispatch(setLoading(false))
+        });
+    }
+    else{
+      const data = {
+        name:Name?.current?.value,
+        email: Email?.current?.value,
+        password: Password?.current?.value,
+      };
+      axios.post("http://localhost:5000/api/v1/register", data, config).then((response) => {
+        // dispatch(setLoading(false))
+        console.log(response.data);
+        enqueueSnackbar("User Successfully Registered, Sign In to continue", {
+          variant: 'success',
+        });
+        // navigate("/");
+        setShowLogin(!showLogin);
+      })
+        .catch((error) => {
+        const msg=error?.response?.data?.error;
+          enqueueSnackbar(msg, {
+            variant: 'error',
+          });
+          // dispatch(setLoading(false))
+        });
+    }
+    
   };
   return (
     <>
@@ -65,18 +93,21 @@ const formToggle=()=>{
           ref={Name}
           className='box-input'      
         />}
+       
         <input
           type="text"
           placeholder="Email"
           ref={Email}
           className='box-input'      
         />
+         {/* {<p style={{color:"red"}}>{errorMsgEmail}</p>} */}
         <input
           type="password"
           placeholder="Password"
           ref={Password}
           
         />
+        {/* {<p style={{color:"red"}}>{errorMsgPassword}</p>} */}
         <button type="submit" onClick={clickHandler}>{showLogin? "Log In" : "Sign Up"}</button>
         <div className="Login_ordiv">
         <div className="Login_divider"></div>
