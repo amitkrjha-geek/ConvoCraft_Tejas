63b62905e660964d2c86b7c92cc7f9a530639bda
Amit Kumar Jha <amit.kumarjha.met19@itbhu.ac.in>
2024-01-12 23:40:05 +0530

    changed user.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,14 +1,14 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 const userSchema = new mongoose.Schema(
-    {
-      phoneNumber: { type: "string", required: false, default:""},
-      name: { type: "string", required: true },
-      email: { type: "string", required: true },
-      profileImageUrl: { type: "string"},
-      password: { type: "string",default:null},
-      googleId: { type: "string", default:null},
-      isVerified: { type: "boolean", default: false}
-    },
-    { timestamps: true }
-  );
-export default mongoose.model("User",userSchema);
+  {
+    phoneNumber: { type: "string", required: false, default: "" },
+    name: { type: "string", required: true },
+    email: { type: "string", required: true },
+    profileImageUrl: { type: "string" },
+    password: { type: "string", default: null },
+    googleId: { type: "string", default: null },
+    isVerified: { type: "boolean", default: false },
+  },
+  { timestamps: true }
+);
+export default mongoose.model("User", userSchema);
